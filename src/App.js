348a059bea1f8fcbd162d6208892d2eefb5ac893
Windana Rahubedde <windana@gmail.com>
2025-08-26 08:41:01 +0530

    dev commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. its working fine.
         </p>
+        <div>
+          All right reserved. 2024
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React How it works and all about it. 
+          Learn React How it works and all about it.  adasdasd
         </a>
       </header>
     </div>
